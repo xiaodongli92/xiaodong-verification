90585c4bef6495344a9bb0935af937e8c571b9ce
lixiaodong <531849939@qq.com>
2016-08-26 14:37:52 +0800

    m

--- src/main/java/com/xiaodong/verification/service/RedisService.java
+++ src/main/java/com/xiaodong/verification/service/RedisService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.Set;
 
 /**
